3f1a4a6cd4a0eff8cd6ac98a60ef47a7b36e7df4
xylophonehero <nick97000@gmail.com>
2022-12-20 07:46:39 +0000

    feat:events

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,9 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import Auth from "../components/Auth";
+import Link from "next/link";
+import { FiHome } from "react-icons/fi";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +15,15 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <header className="flex justify-between items-center gap-2 px-4 py-3">
+        <Link href="/">
+          <FiHome />
+        </Link>
+        <Auth />
+      </header>
+      <main className="flex flex-col items-center justify-center flex-1">
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };

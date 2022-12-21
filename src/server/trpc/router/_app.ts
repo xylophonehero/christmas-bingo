eb59bbf382f4f615075afc718e12034d4ce2e753
xylophonehero <nick97000@gmail.com>
2022-12-21 16:10:20 +0000

    feat:remove ecample router from app

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,9 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { eventRouter } from "./event";
-import { exampleRouter } from "./example";
 import { occasionRouter } from "./occasion";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   event: eventRouter,
   occasion: occasionRouter,
